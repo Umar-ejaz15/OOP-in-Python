7595987dee44db1f5f2678f4b2cc8f47da990e7f
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-03-01 11:04:31 -0800

    oop3

--- oop.py
+++ oop.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Factory:
     def __init__(self, Bt, Et, Tyres):
         self.Bt = Bt
@@ -10,19 +13,52 @@ class Factory:
         print("Tyres: ", self.Tyres)
 
 
-class Honda(Factory):
-    def __init__(self, Bt, Et, Tyres, glass):
+class Factory2:
+    def __init__(self, meterial):
+        self.meterial = meterial
+
+
+class Honda(Factory, Factory2):
+    def __init__(self, Bt, Et, Tyres, glass, meterial):
         super().__init__(Bt, Et, Tyres)
         self.glass = glass
+        self.meterial = meterial
 
     def showdets(self):
         super().showdets()
         print("glass type", self.glass)
+        print("meterial type", self.meterial)
 
 
 farari = Factory("covered", "2 cycles", 4)
 alto = Factory("not covered", "1 cycle", 2)
-supara = Honda("covered", "1 cycle", 2, "glass")
+supara = Honda("covered", "1 cycle", 2, "glass", "iron")
 farari.showdets()
 alto.showdets()
 supara.showdets()
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area():
+        pass
+
+    @abstractmethod
+    def perimeter():
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area():
+        pass
+
+    def perimeter():
+        pass
+
+
+cir1 = Circle(12)
+print(cir1.radius)

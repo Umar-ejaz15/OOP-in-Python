79f1cb5a5dd4be924c317fa44395e3806ad063cd
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-03-01 09:32:56 -0800

    OOp

---
+++ oop.py
@@ -0,0 +1,24 @@
+class Factory:
+    def __init__(self, Bt, Et, Tyres):
+        self.Bt = Bt
+        self.Et = Et
+        self.Tyres = Tyres
+
+    def showdets(self):
+        print("Body Type: ", self.Bt)
+        print("Engine Type: ", self.Et)
+        print("Tyres: ", self.Tyres)
+
+
+class Honda(Factory):
+    def __init__(self, Bt, Et, Tyres, glass):
+        super().__init__(Bt, Et, Tyres)
+        self.glass = glass
+
+
+farari = Factory("covered", "2 cycles", 4)
+alto = Factory("not covered", "1 cycle", 2)
+supara = Honda("covered", "1 cycle", 2, "glass")
+farari.showdets()
+alto.showdets()
+supara.showdets()
